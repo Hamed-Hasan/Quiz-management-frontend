e495e7a1693f07dcacb20e2939bad8aace21d850
Hamed Hasan <hamedhasan422421@gmail.com>
2023-12-22 20:15:26 +0300

    🌐 Added cn function for merging tailwind classes. 🔄 Refactored Providers component for Redux store integration. 🚀 Implemented Providers wrapper for global Redux state. 🔧 Fixed issues in cn function for better class merging. 🔍 Updated Providers to use 'use client' instead of 'useClient'. 🛠️ Adjusted Providers to accept children prop explicitly.

---
+++ lib/Providers.tsx
@@ -0,0 +1,13 @@
+"use client";
+import { store } from "@/redux/store";
+import { Provider } from "react-redux";
+
+const Providers = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <Provider store={store}>
+        {children}
+    </Provider>
+  );
+};
+
+export default Providers;
