da3c9b448d06c70fb649b28738491e36260d217a
Hamed Hasan <hamedhasan422421@gmail.com>
2023-12-22 20:14:07 +0300

    🔧 Configured Redux store with middleware for optimal performance. 🔄 Restructured codebase to improve maintainability. 🌐 Integrated React-Redux hooks for seamless state management. 🚀 Added initial reducer structure for future features. 🎨 Refactored code to enhance readability and consistency.

---
+++ redux/store.ts
@@ -0,0 +1,15 @@
+import { reducer } from './rootReducer';
+import { configureStore } from '@reduxjs/toolkit'
+
+export const store = configureStore({
+  reducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware(),
+})
+
+
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch
